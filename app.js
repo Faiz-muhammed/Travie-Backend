41cae2acefe441f5439d8196ee8a686d0bb1b75b
faiz-muhammed <fmuhammedfaiz99@gmail.com>
2022-04-06 12:27:54 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+const db = require("./config/connection");
+const adminRoute = require("./Routes/admin");
+const userRoute = require("./Routes/user");
+const path = require("path");
+
+env.config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: [],
+    credentials: true,
+  })
+);
+
+db.connect((err) => {
+  err
+    ? console.error("error in db conncetion")
+    : console.log("Database connected");
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, (err) => {
+  err
+    ? console.error("Port configuration error:", err)
+    : console.log(`server listening to :${PORT}`);
+});
+
+module.exports = app;
